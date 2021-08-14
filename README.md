49929aef7327a6fbe79217dc3fb4ccea6116ac14
Robin Mueller <robin.mueller.m@gmail.com>
2021-08-14 18:11:07 +0200

    updated deployment script

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
-Cross-compiling Rust for the Raspberry PI
+Cross-compiling Rust for the Raspberry Pi
 ======
+
+# Build with `cargo`
+
+Build debug image:
+
+```sh
+cargo build --target armv7-unknown-linux-gnueabihf
+```
 
 # Prerequisites
 
-1. Install standard library
+1. Install rust on your development machine using `rustup`
+
+   ```sh
+   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+   ```
+
+2. Install standard library
 
 	```sh
 	rustup target add armv7-unknown-linux-gnueabihf
    ```
 
-2. Install a cross-compile toolchain and add it to your path.
+3. Install a cross-compile toolchain and add it to your path.
    You can download one built with crosstool-ng from
    [here](https://www.dropbox.com/sh/hkn4lw87zr002fh/AAAO-HxFQzfmmPQQ9KVmoooGa?dl=0).
+
+4. Install `sshpass`
+
+   ```sh
+   sudo apt-get install sshpass
+   ```
--- deploy-rpi.sh
+++ deploy-rpi.sh
@@ -1,11 +1,95 @@
 #!/bin/bash
-pi_ip="raspberrypi.local"
+pi_user="pi"
+pi_addr="raspberrypi.local"
+scriptname="deploy-rpi.sh"
+app_name="rpi-rs-crosscompile"
+
+read -r -d '' cli_help <<EOD
+Raspberry Pi Rust Deployment Helper
+
+Builds the image and can optionally transfer it to the target system as well.
+
+Usage:
+    ${scriptname} [flags] [options]
+
+Flags:
+    -p, --release           Build production image
+    -t, --transfer		    Transfer image to Q7S
+    -r, --run               Run the image
+
+Options:
+    -f, --sshfile FILE      SSH key file. Otherwise, use password from
+                            environmental variable SSHPASS
+    -e, --sshenv            Take password from environmental variable
+EOD
+
+arg_transfer=n
+arg_help=n
+arg_run=n
+arg_ssh_env=n
+arg_ssh_file=n
+arg_release=n
+sshkey_file=""
+
+while (( "${#}" )); do
+case "${1}" in
+    -t|--transfer)
+        arg_help=y
+        shift
+        ;;
+    -h|--help)
+        arg_help=y
+        shift
+        ;;
+    -r|--run)
+        arg_run=y
+        shift
+        ;;
+    -e|--sshenv)
+        arg_ssh_env=y
+        shift
+        ;;
+    -p|--release)
+        arg_release=y
+        shift
+        ;;
+    -f|--sshfile)
+        arg_ssh_file=y
+        if [ "${#}" -ge 2 ]; then
+            sshkey_file="${2}"
+        else
+            echo "Error: Missing argument for ${1}"
+            exit 1
+        fi
+        shift
+        ;;
+    esac
+done
+
+# handle help
+if [ ${arg_help} = y ]; then
+    echo "${cli_help}"
+    exit 0
+fi
+
+cargo_opts=""
+if [ arg_release = y ]; then
+    cargo_opts+="--release"
+fi
+
 target="armv7-unknown-linux-gnueabihf"
+cargo build ${cargo_opts} --target ${target}
 
-cargo build --target ${target}
+sshpass_args=""
+if [ ${arg_ssh_file} = y ]; then
+    sshpass_args="-f ${sshkey_file}"
+elif [ ${arg_ssh_env} = y ]; then
+    sshpass_args="-e"
+fi
 
-sshpass -e scp \
-	-r ./target/${target}/debug/rpi-rs-crosscompile \
-	pi@${pi_ip}:/home/pi
-
-sshpass -e ssh pi@${pi_ip} './rpi-rs-crosscompile'
+if [ arg_transfer = y ]; then
+	sshpass ${sshpass_args} q7s-cp.py ./target/${target}/debug/${app_name}
+    if [ arg_run = y ]; then
+        sshpass ${sshpass_args} ssh -p 1535 ${pi_user}@${pi_addr} "./${app_name}"
+    fi
+fi
